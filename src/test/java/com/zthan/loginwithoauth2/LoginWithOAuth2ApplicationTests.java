e26608d9f2bebb333aa9b64bd67999a09eb1515b
Zaw Than <zawlurnthan@gmail.com>
2023-05-21 20:17:56 -0400

    Initial commit

---
+++ src/test/java/com/zthan/loginwithoauth2/LoginWithOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zthan.loginwithoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithOAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
